311af273951b7dae2c3cd2dd2d3f2f94a7be4a3f
farid-fari <7dare@outlook.com>
2017-06-03 13:34:13 +0200

    added bold and italics, and updated example

--- examples/one.md
+++ examples/one.md
@@ -1,5 +1,29 @@
 # Introduction
 
+We will introduce the **subject** matter here.
+
+## First part
+
+The first aspect is discussed *here*.
+
+### About the World
+
+It's a damn scary place.
+
+#### Hellos in relation to the world
+
+They're huge.
+
+##### The relation between H and W
+
+I'm uninspired.
+
+###### Unicode encoding of said letters
+
+I never remember it.
+
+####### This shouldn't be any more indented
+
 Hello *world*, **here** I come...
 
 - Hello
--- mdtotex.py
+++ mdtotex.py
@@ -10,7 +10,7 @@ import re
 # Checks if the previous files exists and opens it
 filename = sys.argv[1]
 if not os.path.exists(filename):
-    raise FileNotFoundError
+    raise FileNotFoundError("Couldn't find target")
 f = open(filename, 'r')
 
 # Generates the new filename and opens it,
@@ -18,27 +18,55 @@ f = open(filename, 'r')
 newfile = os.path.splitext(filename)
 newfile = newfile[0] + ".tex"
 if os.path.exists(newfile):
-    raise FileExistsError
+    raise FileExistsError("Target file exists")
 n = open(newfile, 'w')
 print(f"Writing to {newfile}...")
 
+# Initial text
+n.write(r'\documentclass{article}' + '\n')
+## Noone wants indented paragraphs in md
+n.write(r'\setlength{\parindent}{0pt}')
+n.write(r'\begin{document}' + '\n\n')
+
 for l in f.readlines():
-    # Amount of open brackets
-    o = 0
+    # Amount of open brackets, parenthesis and square
+    # brackets respectively
+    o = [0, 0, 0]
+
+    # Title detection
     title = re.search(r"^\#{1,6}", l)
     if title:
         importance = len(title.group(0))
-        titlefmts = ['', 'part', 'chapter', 'section',
-                     'subsection', 'subsubsection', 'paragraph']
+        titlefmts = ['', 'part*', 'section*', 'subsection*',
+                     'subsubsection*', 'paragraph', 'subparagraph']
         n.write("\\" + titlefmts[importance] + "{")
+        o[0] += 1
         l = re.sub(r"^\#{1,6}[ ]?", "", l)
+        # Escape any remaining hashes
+        l = re.sub("#", r'\\#', l)
+
+    # Bold text detection
+    l = re.sub(r"\*\*(.*?)\*\*", r'\\textbf{\1}', l)
+    # Italic text detection
+    l = re.sub(r"\*(.*?)\*", r'\\textit{\1}', l)
 
     n.write(l.rstrip())
 
     if title:
         n.write("}")
+        if importance >= 5:
+            # Paragraphs and subparagraphs
+            # don't newline
+            n.write(r'\mbox{}\newline')
+        o[0] -= 1
 
     n.write('\n')
+
+    if sum(o) != 0:
+        raise AssertionError("Bracket problem")
+
+# End of the document
+n.write('\n' + r'\end{document}' + '\n')
 
 f.close()
 n.close()
